0eb36737842c201a7ea4a091f65c5b3fc94a2b5d
Radek Józefowicz <radoslawjozefowicz@gmail.com>
2021-05-10 23:51:16 +0200

    query-users - added try-catch

--- query-users/app.js
+++ query-users/app.js
@@ -2,41 +2,48 @@ const AWS = require("aws-sdk");
 const client = new AWS.DynamoDB.DocumentClient();
 
 const headers = {
-    "Access-Control-Allow-Headers" : "*",
+    "Access-Control-Allow-Headers": "*",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "*"
 };
 
 exports.lambdaHandler = async (event, context) => {
-
-    if (event.path === '/users') {
-        const result = await client.scan({
-            TableName: process.env.USERS_TABLE
-        }).promise();
-        return {
-            headers,
-            'statusCode': 200,
-            body: JSON.stringify(result.Items)
-        }
-    } else {
-        const id = event.pathParameters.id;
-        const result = await client.get({
-            TableName: process.env.USERS_TABLE,
-            Key: {
-                id
+    try {
+        if (event.path === '/users/{id}') {
+            const id = event.pathParameters.id;
+            const result = await client.get({
+                TableName: process.env.USERS_TABLE,
+                Key: {
+                    id
+                }
+            }).promise();
+            if (result.Item) {
+                return {
+                    headers,
+                    'statusCode': 200,
+                    body: JSON.stringify(result.Item)
+                }
+            } else {
+                return {
+                    headers,
+                    'statusCode': 404
+                }
             }
-        }).promise();
-        if (result.Item) {
+        } else {
+            const result = await client.scan({
+                TableName: process.env.USERS_TABLE
+            }).promise();
             return {
                 headers,
                 'statusCode': 200,
-                body: JSON.stringify(result.Item)
+                body: JSON.stringify(result.Items)
             }
-        } else {
-            return {
-                headers,
-                'statusCode': 404
-            }
+        }
+    } catch (e) {
+        console.log(e);
+        return {
+            headers,
+            'statusCode': 500
         }
     }
 };
